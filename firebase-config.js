e87da738b3c87218cce44d8d6704fdb4e69a6fa8
99aisolutions-oss <99aisolutions@gmail.com>
2026-06-02 20:41:46 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,14 @@
 // Relais IA - Configuration Firebase
 // Remplace les valeurs ci-dessous par le bloc firebaseConfig donné par Firebase Console.
 // L'application fonctionne aussi en mode démo local si ces champs restent vides.
+// Relais IA - Configuration Firebase
 
 window.RELAIS_IA_FIREBASE_CONFIG = {
-  apiKey: "",
+  apiKey: "AIzaSyDrhtsH5ucfPw88-3M0lp7h60Sw-RTBnyM",
   authDomain: "relais-ia.firebaseapp.com",
   projectId: "relais-ia",
   storageBucket: "relais-ia.firebasestorage.app",
   messagingSenderId: "873664928347",
-  appId: ""
+  appId: "1:873664928347:web:87ec8c67db6342acf3bd7e"
 };
+  
